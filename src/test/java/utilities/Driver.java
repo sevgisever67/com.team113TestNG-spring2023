d0ffafb019d18430955ed0d654394015c0b426f0
sevgi67 <elizafirs0@gmail.com>
2023-05-24 23:10:54 +0300

    23/05/23

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -13,7 +13,7 @@ public class Driver {
     private static WebDriver driver;
     private Driver(){
         /* Singleton pattern kullanilarak istenmeyen yontemlerle
-           driver objesine erisilmesini engelledik
+           driver objesine erisilmesini engelledik.
 
            Constructor'i private yaparak bu class'dan obje olusturularak
            class uyelerinin kullanilmasinin onune gectik
